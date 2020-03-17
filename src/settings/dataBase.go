6bf15a425df08e4f81567f570952f357d6c504a4
Leonardo Antonio Miranda <leoamiranda2@gmail.com>
2020-03-17 17:18:31 -0300

    environment database on docker

--- src/settings/dataBase.go
+++ src/settings/dataBase.go
@@ -8,10 +8,10 @@ import (
 
 func ConnectDB() *gorm.DB {
 
-	var host string = os.Getenv("DBHOSTGOLANG")
-	var user string = "root"
-	var password string = "root"
-	var dbname string = "golang_mysql"
+	var host string = os.Getenv("DB_HOST_GOLANG")
+	var user string = os.Getenv("DB_USER_GOLANG")
+	var password string = os.Getenv("DB_PASSWORD_GOLANG")
+	var dbname string = os.Getenv("DB_NAME_GOLANG")
 
 	db, err := gorm.Open("mysql", user+":"+password+"@("+host+")/"+dbname+"?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
